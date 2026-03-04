880369a680e901a92bc5ef22a672dcc111256fc5
GIOVANA BARBOSA SOUZA <25133664@dsn127m1242314.cfp127aluno.local>
2026-03-04 17:00:05 -0300

    colocando os componentes na tela

--- app/src/main/java/com/aulasandroid/veridicadordeidade/MainActivity.kt
+++ app/src/main/java/com/aulasandroid/veridicadordeidade/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.aulasandroid.veridicadordeidade
 
+import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.aulasandroid.veridicadordeidade.ui.theme.VeridicadorDeIdadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,6 +43,53 @@ fun verificarIdade (modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(Color.White)
-    ) { }
+            .background(Color.White),
+
+
+    ) {
+        Column( modifier = Modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+
+            Column( modifier = Modifier,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                Text( modifier = Modifier
+                    .fillMaxWidth()
+                    ,
+                    text = "Qual é a sua idade?",
+                    color = Color.Blue,
+                    fontSize = 32.sp
+                )
+                Text( modifier = Modifier
+                    .fillMaxWidth(),
+                    text = "Aperte os botões para informar sua idade",
+                    color = Color.Black
+                )
+            }
+
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    onClick = {},
+
+                ) {
+                    Text( text = "-")
+                }
+
+                Button(
+                    onClick = {},
+
+                    ) {
+                    Text( text = "+")
+                }
+
+            }
+            Text(text = "Você é ... de idade")
+        }
+    }
 }
