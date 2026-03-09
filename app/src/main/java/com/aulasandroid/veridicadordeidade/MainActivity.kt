a8380ad1653bfaf9afa0950d189ea1f77d3d053c
GIOVANA BARBOSA SOUZA <25133664@dsn127m1242314.cfp127aluno.local>
2026-03-09 15:59:26 -0300

    atividade finalizada

--- app/src/main/java/com/aulasandroid/veridicadordeidade/MainActivity.kt
+++ app/src/main/java/com/aulasandroid/veridicadordeidade/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.aulasandroid.veridicadordeidade
 
+import android.R.attr.onClick
 import android.R.attr.text
 import android.os.Bundle
+import android.util.Log.i
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,13 +11,26 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -40,56 +55,101 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun verificarIdade (modifier: Modifier = Modifier) {
+
+    var idade by remember {
+        mutableIntStateOf(0)
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize()
             .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
 
 
     ) {
-        Column( modifier = Modifier,
-            horizontalAlignment = Alignment.CenterHorizontally
 
-        ) {
-
-            Column( modifier = Modifier,
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                Text( modifier = Modifier
-                    .fillMaxWidth()
-                    ,
+            //Spacer(modifier = Modifier.height(12.dp))
+            Row ( modifier = Modifier
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center) {
+                Text(
                     text = "Qual é a sua idade?",
                     color = Color.Blue,
                     fontSize = 32.sp
                 )
-                Text( modifier = Modifier
-                    .fillMaxWidth(),
+            }
+            Row(modifier = Modifier
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+                ) {
+                Text(
                     text = "Aperte os botões para informar sua idade",
                     color = Color.Black
                 )
             }
 
 
+        Spacer(modifier = Modifier.height(16.dp))
+
+            Row(modifier = Modifier
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "${idade}",
+                    fontSize = 32.sp
+
+                    )
+            }
+        Spacer(modifier = Modifier.height(16.dp))
+
             Row(
                 modifier = Modifier
                     .fillMaxWidth(),
                 horizontalArrangement = Arrangement.Center
-            ) {
-                Button(
-                    onClick = {},
 
-                ) {
-                    Text( text = "-")
+            ) {
+
+                Button(
+
+                    onClick = { idade --},
+                    shape = RoundedCornerShape( 10.dp, 10.dp, 10.dp, 10.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(63, 67, 197, 255)
+                    )
+
+                ){
+                    Text( text = "-",
+                        fontSize = 24.sp)
                 }
 
-                Button(
-                    onClick = {},
+            Spacer(modifier = Modifier.width(10.dp))
 
+                Button(
+                    onClick = {idade ++},
+                    shape = RoundedCornerShape( 10.dp, 10.dp, 10.dp, 10.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(63, 67, 197, 255)
+                    )
                     ) {
-                    Text( text = "+")
+                    Text( text = "+",
+                        fontSize = 24.sp)
                 }
 
             }
-            Text(text = "Você é ... de idade")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(modifier = Modifier) {
+            Text(
+                text = "Você é ${if ( idade >= 18 ) "MAIOR" else if ( idade <= -1 || idade > 180) "XXX" else "MENOR" } de idade",
+                fontSize = 24.sp,
+                color = Color.Blue
+            )
+
         }
     }
 }
+
